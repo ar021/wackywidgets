4055e1085af120f047ccb7a74c1ee00a48032c25
Avesh Rathod <avesh@Aveshs-MacBook-Air.local>
2021-07-06 20:35:32 -0400

    functionality complete

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from main_app.models import Widget
+from django.shortcuts import render, redirect
+from django.views.generic.edit import CreateView
+from .models import Widget
+from .forms import WidgetForm
 
-# Create your views here.
+
+
+# Define the home view
+def index(request):
+    widgets = Widget.objects.all()
+    widget_form = WidgetForm
+    return render(request, 'index.html', {'widgets' : widgets, 'widget_form' : widget_form })
+
+def add_widget(request):
+    form = WidgetForm(request.POST)
+    if form.is_valid():
+        new_widget = form.save(commit=False)
+        new_widget.save()
+    return redirect('index')
+
+def delete_widget(request, widget_id):
+    w = Widget.objects.get(id=widget_id)
+    w.delete()
+    return redirect('index')
